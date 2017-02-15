cac0f8cb97b1081b1b7ab489cec9122eeb738618
Stephanie <chaifae@gmail.com>
2017-02-14 19:50:57 -0700

    update project

--- project.py
+++ project.py
@@ -462,6 +462,6 @@ def createUser(login_session):
     return user.id
 
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     app.debug = True
+#     app.run(host='0.0.0.0', port=5000)
