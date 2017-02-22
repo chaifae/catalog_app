2d245547f69cb874534a0f82db397a4d9f40193a
Stephanie <chaifae@gmail.com>
2017-02-21 20:01:49 -0700

    attempt

--- project.py
+++ project.py
@@ -339,8 +339,7 @@ def fbconnect():
     login_session['facebook_id'] = data['id']
 
     # get user picture
-    url = '''https://graph.facebook.com/v2.4/me/picture?%s&redirect
-        =0&height=200&width=200''' % token
+    url = "https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200" % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
@@ -372,8 +371,7 @@ def fbdisconnect():
     facebook_id = login_session['facebook_id']
     # access token must be included to successfully log out
     access_token = login_session['access_token']
-    url = '''https://graph.facebook.com/%s/permissions?
-        access_token=%s''' % (facebook_id, access_token)
+    url = "https://graph.facebook.com/%s/permissions?access_token=%s" % (facebook_id, access_token)
     h = httplib2.Http()
     result = h.request(url, 'DELETE')[1]
 
