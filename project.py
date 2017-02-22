8f1d0de1d073096d7bc82707fd99b1f0a2bfa95d
Stephanie <chaifae@gmail.com>
2017-02-21 19:59:00 -0700

    attempt

--- project.py
+++ project.py
@@ -317,9 +317,7 @@ def fbconnect():
         open('/var/www/html/catalog_app/fb_client_secrets.json', 'r').read())['web']['app_id']
     app_secret = json.loads(
         open('/var/www/html/catalog_app/fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = '''https://graph.facebook.com/oauth/access_token?grant_type=
-        fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token
-        =%s''' % (app_id, app_secret, access_token)
+    url = "https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s" % (app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
